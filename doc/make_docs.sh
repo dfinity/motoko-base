c29ba9938d663f393e7f8aee25b1999af4383469
Christoph Hegemann <6189397+kritzcreek@users.noreply.github.com>
2020-08-05 13:22:44 +0200

    Update vessel version and uses compiler binary tarballs (#157)
    * Updates tests to vessel with dhall
    
    * use binaries from Motoko releases
    
    * creates directory for binaries on CI
    
    * no longer needs dfx cache install

--- doc/make_docs.sh
+++ doc/make_docs.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 rm -rf _out
 mkdir -p _out
-../bin/mo-doc --source ../src --output _out/adoc --format adoc
-../bin/mo-doc --source ../src --output _out/html --format html
+mo-doc --source ../src --output _out/adoc --format adoc
+mo-doc --source ../src --output _out/html --format html
