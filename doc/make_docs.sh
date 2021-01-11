cf7f2d1ce1a37accdf8553f2a3abf0181a4b7025
chenyan-dfinity <yan.chen@dfinity.org>
2021-01-11 13:12:02 -0800

    fix

--- doc/make_docs.sh
+++ doc/make_docs.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mo-doc --source ../src --output modules/base-libraries/pages/ --format adoc
+rm -rf _out
+mkdir -p _out
+mo-doc --source ../src --output _out/adoc --format adoc
+mo-doc --source ../src --output _out/html --format html
