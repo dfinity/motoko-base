39a2eb88c0683652908ca67568ba521722ea2cba
chenyan-dfinity <yan.chen@dfinity.org>
2021-01-11 17:18:27 -0800

    proper sorting

--- doc/module_nav.py
+++ doc/module_nav.py
@@ -10,7 +10,16 @@ for file in glob.glob("src/**/*.mo", recursive=True):
   module = os.path.splitext(os.path.basename(file))[0];
   names.append(module);
 
-names.sort();
+def sort_name(name):
+    head = name.rstrip('0123456789')
+    tail = name[len(head):]
+    if tail:
+        tail = int(tail)
+    else:
+        tail = 0
+    return (head, tail)
+  
+names.sort(key=sort_name);
 print("* xref:stdlib-intro.adoc[Motoko Base Library]")
 for name in names:
     print(f"** xref:./{name}.adoc[{name}]")
