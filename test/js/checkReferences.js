f884484bae32412988860c78a10f6f678332155b
rvanasa <ryan.vandersmith@dfinity.org>
2022-12-28 17:42:32 -0700

    Add 'mo:base' import sanity check

---
+++ test/js/checkReferences.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const { join } = require("path");
+const { readFileSync } = require("fs");
+const glob = require("fast-glob");
+
+const srcDirectory = join(__dirname, "../../src");
+
+// Detect "mo:base/..." imports within the base library itself (important for )
+
+glob.sync(join(srcDirectory, "**/*.mo")).forEach((srcPath) => {
+  const source = readFileSync(srcPath, "utf8");
+  source.split("\n").forEach((line, i) => {
+    line = line.trim();
+    if (
+      line.includes('"mo:base') &&
+      !line.startsWith("//") &&
+      !line.endsWith("// ignore-reference-check")
+    ) {
+      console.error(
+        `A possible reference to \`mo:base\` was found at ${srcPath}:${
+          i + 1
+        }\n\nIf this was intentional, consider adding an \`// ignore-reference-check\` comment to the end of the line.\nOtherwise, try removing the "mo:base/..." prefix in favor of a relative path.`
+      );
+      process.exit(1);
+    }
+  });
+});
