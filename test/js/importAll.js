50b987620ebce7ab1998ad859af1754f9d849480
rvanasa <ryan.vandersmith@dfinity.org>
2024-04-03 10:43:39 -0600

    Fix CI

--- test/js/importAll.js
+++ test/js/importAll.js
@@ -19,23 +19,26 @@ const moFiles = glob.sync("**/*.mo", { cwd: resolve(__dirname, "../../src") });
 if (moFiles.length === 0) {
   throw new Error("Expected at least one Motoko file in `src` directory");
 }
-const source =
-  moFiles
-    .map((f) => {
-      const name = f.replace(/\.mo$/, "");
-      return `import _${name} "../../src/${name}";\n`;
-    })
-    .join("") + "\nactor {};";
+const source = moFiles
+  .map((f) => {
+    const name = f.replace(/\.mo$/, "");
+    return `import _${name} "../../src/${name}";\n`;
+  })
+  .join("");
 
 writeFileSync(outFile, source, "utf8");
 
 (async () => {
   const mocPath = process.env.DFX_MOC_PATH || "moc";
   const wasmFile = join(outDir, `${baseFilename}.wasm`);
-  const { stdout, stderr } = await execa(mocPath, [outFile, "-o", wasmFile], {
-    stdio: "pipe",
-    encoding: "utf8",
-  });
+  const { stdout, stderr } = await execa(
+    mocPath,
+    [outFile, "-wasi-system-api", "-o", wasmFile],
+    {
+      stdio: "pipe",
+      encoding: "utf8",
+    }
+  );
   console.log(stdout);
   if (stderr.trim()) {
     throw new Error(`Warning message while importing modules:\n${stderr}`);
