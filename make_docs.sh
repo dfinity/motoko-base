f5ffc60a36d8d8c2e6dc388878a8cfc1c8ace5bc
jessiemongeon1 <133128541+jessiemongeon1@users.noreply.github.com>
2025-06-05 13:05:48 +0000

    Deploying to doc-pages from  @ ae82bc056c14ff3bb546e008c4813d7a37b63659 🚀

--- make_docs.sh
+++ make_docs.sh
@@ -3,6 +3,4 @@ set -euo pipefail
 
 rm -rf _out
 mkdir -p _out
-mo-doc --source ../src --output _out/adoc --format adoc
-mo-doc --source ../src --output _out/html --format html
 mo-doc --source ../src --output _out/md --format plain
